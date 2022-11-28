c46c2f0a220bef2f8dbfddea6156691c4bea4f58
Luan <luan@luan-desktop>
2022-11-28 17:34:59 -0300

    now using react router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/main.css';
+import { GameAds } from './pages/GameAds';
+import { Home } from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/game/:gameName',
+    element: <GameAds />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
