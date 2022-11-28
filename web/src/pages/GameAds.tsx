6669fd8bfbae84af1b0eca6f00f69a339ac2c955
Luan <luan@luan-desktop>
2022-11-28 17:35:32 -0300

    moved pages to this folder

---
+++ web/src/pages/GameAds.tsx
@@ -0,0 +1,12 @@
+import { GameHeader } from "../components/GameHeader";
+
+
+export function GameAds() {
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-start my-20'>
+      <GameHeader
+        bannerUrl={"https://static-cdn.jtvnw.net/ttv-boxart/32399_IGDB-285x380.jpg"}
+        title={"Counter Strike: GO"}/>
+    </div>
+  )
+}
