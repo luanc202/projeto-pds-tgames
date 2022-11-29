fc4959f2ce5067e15e5308453f81b1f78d8f47f2
Luan <barkon32@tuta.io>
2022-11-29 15:43:12 -0300

    added files for the upcoming components

---
+++ web/src/components/CreateAccountModal.tsx
@@ -0,0 +1,9 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+export function CreateAccountModal() {
+    return (
+        <div className='max-w-[1344px] mx-auto flex flex-col items-start my-20'>
+          <h1 className="text-6xl text-white">O componente de criar conta para um usuário deve aparecer aqui</h1>
+        </div>
+      )
+}
---
+++ web/src/pages/AdminPanel.tsx
@@ -0,0 +1,7 @@
+export function AdminPanel() {
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-start my-20'>
+      <h1 className="text-6xl text-white">O painel administrativo deve aparecer aqui</h1>
+    </div>
+  )
+}
