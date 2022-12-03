4fe37b46bb34c093e8c8c47fefc254323c6c2e32
Luan <luan@luan-desktop>
2022-12-02 23:40:04 -0300

    added admin user model to schema

---
+++ server/prisma/migrations/20221203023953_added_admin_user_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "AdminUser" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminUser_email_key" ON "AdminUser"("email");
